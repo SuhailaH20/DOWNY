18b9967aca9a259f60bd07bb59cca7ef28c7c5bc
Suhaylah <suhailahawsawi@gmail.com>
2025-11-25 13:39:58 +0300

    MVVM structure

---
+++ DOWNY/views/boardPage.swift
@@ -0,0 +1,20 @@
+//
+//  boardPage.swift
+//  DOWNY
+//
+//  Created by Suhaylah hawsawi on 04/06/1447 AH.
+//
+
+import SwiftUI
+
+struct boardPage: View {
+    var body: some View {
+        ZStack {
+            backgroundImage()
+        }
+    }
+}
+
+#Preview {
+    boardPage()
+}
